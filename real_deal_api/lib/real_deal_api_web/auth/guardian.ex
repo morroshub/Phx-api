3a95e610f3ab05bed9b9341b90b6ae749f142caf
morroshub <lgmorrone@icloud.com>
2023-12-16 14:34:16 -0300

    se anadieron funciones publicas y privadas  dentro de aut/guardian def-(authenticate) defp (validate_password,create_token)

--- real_deal_api/lib/real_deal_api_web/auth/guardian.ex
+++ real_deal_api/lib/real_deal_api_web/auth/guardian.ex
@@ -22,6 +22,25 @@ defmodule RealDealApiWeb.Auth.Guardian do
   def resource_from_claims(_claims) do
     {:error, :no_id_provided}
   end
+
+  def authenticate(email, password) do
+    case Accounts.get_account_by_email(email) do
+      nil -> {:error, :unauthored}
+      account ->
+        case validate_pasword(password, account.hash_password) do
+          true -> create_token(account)
+          false -> {:error, :unauthored}
+ 
+  end
 end
 
+  defp validate_password(password, hash_password) do
+    Bcrypt.verify_pass(password, hash_password)
+ end
+
+  defp create_token(account) do
+    {:ok, token, _claims} = encode_and_sing(account)
+    {:ok, account, token}
+  end
+end 
 
