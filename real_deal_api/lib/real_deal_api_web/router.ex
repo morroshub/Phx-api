3b81db43a221b0eb030e4aaf7c728265480448d1
morroshub <lgmorrone@icloud.com>
2023-12-19 12:47:06 -0300

    se crean los archivos pipeline y guardian_error_hanler dentro de /auth, se agregan funciones en router.ex. Testeado y funcionando para obtener id de cuenta con el JWT

--- real_deal_api/lib/real_deal_api_web/router.ex
+++ real_deal_api/lib/real_deal_api_web/router.ex
@@ -25,10 +25,23 @@ defmodule RealDealApiWeb.Router do
     plug :accepts, ["json"]
   end
 
+
+  pipeline :auth do
+    plug RealDealApiWeb.Auth.Pipeline
+  end
+
   scope "/api", RealDealApiWeb do
     pipe_through :api
     get "/", DefaultController, :index
     post "/accounts/create", AccountController, :create
     post "/accounts/sign_in", AccountController, :sign_in
+  end
+
+
+  # Requisito de JWT para nuestros endpoints desde las api
+  scope "/api", RealDealApiWeb do
+    pipe_through [:api, :auth]
+    get "/accounts/by_id/:id", AccountController, :show
+
   end
 end
