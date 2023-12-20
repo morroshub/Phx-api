fca9685225f2aab48f653e90be45f0be191bf3ce
morroshub <lgmorrone@icloud.com>
2023-12-20 16:53:12 -0300

    anadimos la funcion y su route , refresh session para actualizar el jwt de la misma cuenta id

--- real_deal_api/lib/real_deal_api_web/auth/error_response.ex
+++ real_deal_api/lib/real_deal_api_web/auth/error_response.ex
@@ -5,3 +5,7 @@ end
 defmodule RealDealApiWeb.Auth.ErrorResponse.Forbidden do
   defexception [message: "Error : User no authorized.", plug_status: 403]
 end
+
+defmodule RealDealApiWeb.Auth.ErrorResponse.NotFound do
+  defexception [message: "Not Found", plug_status: 404]
+end
